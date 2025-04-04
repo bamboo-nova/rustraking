70e74fd04f11462eada2077d9cc325f8eef038b3
bamboo-nova <suzuk1nov11@gmail.com>
2025-04-04 13:33:45 +0900

    :wrench: fixed args comment and add comment

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Clone)]
 #[command(name = "track", version = "0.1.0", author = "chiikawa", about = "yolo tracking")]
 pub struct Args {
-    /// ONNX model path
+    /// model path
     #[arg(short = 'm', long = "model", value_name = "MODEL", default_value = "yolov8n", help = "ONNX model path")]
     pub model: String,
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // PyErrなので、ここでBox<dyn Error>に戻す
     if let Err(e) = track::get_args().and_then(|args| track::run(args).map_err(|e| Box::new(e) as Box<dyn std::error::Error>)) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
